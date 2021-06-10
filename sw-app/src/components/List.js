da08b35a16c73738c7900f8b85cd87c253403e48
Elisabeth Droeser <elisabethdroeser@gmail.com>
2021-06-10 22:15:31 +0200

    Add start issues with app

---
+++ sw-app/src/components/List.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const List = () => {
+  return <h1>Hello List</h1>
+}
---
+++ sw-app/src/slask.js
@@ -0,0 +1,24 @@
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div main>
+        <section class="accordion">
+            <label for="cb_1"><h2>Rubrik 1</h2></label>
+            <input id="cb_1" type="checkbox"></input>
+            <section>
+                <p>Text</p><p>mer text</p>
+            </section>
+            <label for="cb_2"><h2>Rubrik 2</h2></label>
+            <input id="cb_2" type="checkbox"></input>
+            <section>
+                <p>mer text</p>
+            </section>
+            <label for="cb_3"><h2>Rubrik 3</h2></label>
+            <input id="cb_3" type="checkbox"></input>
+            <section>
+                <p>Text</p>
+            </section>
+        </section>
+    </div>
+      </header>
+    </div>
