91d63c75b0690526dc42f01236abf89156bc871a
Elisabeth Droeser <elisabethdroeser@gmail.com>
2021-06-17 14:07:56 +0200

    Reverse files to fetch data

--- sw-app/src/App.js
+++ sw-app/src/App.js
@@ -1,33 +1,19 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from "react"
+//import React, { useEffect, useState } from 'react'
 import Header from "./components/Header"
-import People from "./components/People"
+import List from "./components/List"
+import { fetchData } from "./utils/Api"
 
-const App = () => {
-  const [people, setPeople] = useState([
-      {
-        id: 1,
-        name: "Luke Skywalker",
-        eye_color: "Blue",
-        birth_year: "19 BBY",
-    },
-    {
-      id: 2,
-      name: "C-3PO",
-      eye_color: "yellow",
-      birth_year: "112BBY",
-    },
-    {
-      id: 3,
-      name: "R2-D2",
-      eye_color: "red",
-      birth_year: "33BBY"
-    }
-  ])
+function App() {
+  const [people, setPeople] = useState([])
+  useEffect(() => {
+    fetchData().then(response => setPeople(response))
+  })
+
   return (
     <div className="container">
       <Header />
-      <People people={people} />
-
+      <List people={people} />
     </div>
   )
 }
--- sw-app/src/utils/Api.js
+++ sw-app/src/utils/Api.js
@@ -1,15 +1,25 @@
 const urls = [
-  //"https://swapi.dev/api/people/1",
+  "https://swapi.dev/api/people/1/",
+  "https://swapi.dev/api/people/2/",
+  "https://swapi.dev/api/people/3/",
+  "https://swapi.dev/api/people/4/",
+  "https://swapi.dev/api/people/5/",
+
 ]
 
 export const fetchData = async () => {
-  try {
-    const response = await Promise.all(
-      urls.map(url => fetch(url).then(res => res.json()))
-    );
-    console.log(response);
-    return response;
-  } catch (error) {
-    console.log(Error, error);
-  }
+    try {
+      const response = await Promise.all(
+        urls.map(url => fetch(url).then(res => res.json()))
+      );
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(Error, error);
+    }
 };
+
+export default fetchData
+
+
+
