faccc1b44d9eb3d650c22eba02aa8555cab87bcc
Elisabeth Droeser <elisabethdroeser@gmail.com>
2021-06-17 21:53:34 +0200

    Update axios and get data

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -1,21 +1,39 @@
-import React, { useEffect, useState } from "react"
-import Header from "./components/Header"
+//import React, { useEffect, useState } from "react"
+import React, { useState } from 'react'
+import Axios from 'axios'
+import Header from './components/Header'
+
 //import List from "./components/List"
-import { fetchData } from "./utils/Api"
+//import { fetchData } from "./utils/Api"
 
 function App() {
-  const [people, setPeople] = useState(0);
-  const [person, setPerson] = useState(null);
+  const [people, setPeople] = useState("")
+
+
+  const getPeople = () => {
+    Axios
+    .get('https://swapi.dev/api/people/')
+    .then((response) => console.log(response.data.results))
+    //.then(({ data:results }) => console.log(results))
+  }
+
+
+
+  //const [people, setPeople] = useState(0);
+  /*const [person, setPerson] = useState(null);
 
   useEffect(() => {
     fetchData();
     }, []);
-
+*/
 
 
   return (
     <div className="container">
       <Header />
+      <button onClick={getPeople}>Get People</button>{people}
+
+
     </div>
   )
 }
--- star-wars-app/src/utils/Api.js
+++ star-wars-app/src/utils/Api.js
@@ -2,7 +2,6 @@ export const fetchData = () => {
   return fetch('https://swapi.dev/api/people/')
     .then((response) => response.json())
     .then((data) => console.log(data.results))
-
   }
 
 export default fetchData
