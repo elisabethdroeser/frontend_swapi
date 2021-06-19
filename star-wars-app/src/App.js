a603c2bdf73a9fc94a9b80bb110f12ea05c80ff5
Elisabeth Droeser <elisabethdroeser@gmail.com>
2021-06-19 13:09:07 +0200

    Pagination work

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -1,35 +1,55 @@
 //import React, { useEffect, useState } from "react"
 import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
+import People from './components/People'
+import Pagination from './components/Pagination'
 import axios from 'axios';
 
 //import List from "./components/List"
 //import { fetchData } from "./utils/Api"
 
 function App() {
+  const [people, setPeople] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [peoplePerPage, setPeoplePerPage] = useState(5)
 
-  const myStyle = {
-    backgroundColor: "#f4f4f4",
-    margin: "5px",
-    padding: "10px 20px",
-  }
-  const [people, updatePeople] = useState([])
-  const apiEndpoint =' https://swapi.dev/api/people/';
-    useEffect(() => {
-      axios.get(apiEndpoint)
-      .then((data) => updatePeople({...data.data }))
-      //.then((data) => console.log(data))
-    }, []);
+  useEffect(() => {
+    const fetchPeople = async () => {
+      setLoading(true);
+      const res = await axios.get('https://swapi.dev/api/people/');
+      setPeople(res.data.results);
+      setLoading(false);
+    }
+    fetchPeople();
+  }, []);
+
+//console.log(people);
+
+  //const myStyle = {
+  //  backgroundColor: "#f4f4f4",
+  //  margin: "5px",
+  //  padding: "10px 20px",
+  //}
+  //const [people, updatePeople] = useState([])
+  //const apiEndpoint = 'https://swapi.dev/api/people/';
+  //  useEffect(() => {
+  //    axios.get(apiEndpoint)
+  //    .then((data) => updatePeople({...data.data }))
+  //    //.then((data) => console.log(data))
+  //  }, []);
+//
+
+const indexOfLastPeople= currentPage * peoplePerPage;
+const indexOfFirstPeople = indexOfLastPeople - peoplePerPage;
+const currentPeople = people.slice(indexOfFirstPeople, indexOfLastPeople);
+
 
   return (
     <div className="container">
       <Header />
-      {people.results && people.results.map(person => (
-        <div key={person.div} style={myStyle}>
-          <p>{person.name}</p>
-        </div>
-      ))}
-
+      <People people={currentPeople} loading={loading} />
+      <Pagination peoplePerPage={peoplePerPage} totalPeople={people.length} />
     </div>
   )
 }
--- star-wars-app/src/components/People.js
+++ star-wars-app/src/components/People.js
@@ -1,13 +1,18 @@
 import React from 'react'
-//import Item from ".components/Item"
 
-const People = ({ people }) => {
+const People = ({ people, loading }) => {
+  if(loading) {
+    return <h2>Lista med människor</h2>
+  }
   return (
     <div>
-      <h2>Starwars personer</h2>
-      <ul>
+      <h2>Starwars</h2>
+      <ul className="list-group mb-4">
         {people.map(person => {
-          return <li key={person.id}>{person.name}</li>
+          return <li key={person.id} className="list-group-item">
+            {person.name}
+            {person.birth_year}
+          </li>
         })}
       </ul>
     </div>
@@ -15,3 +20,21 @@ const People = ({ people }) => {
 }
 
 export default People
+
+
+
+
+//const People = ({ people }) => {
+//  return (
+//    <div>
+//      <h2>Starwars personer</h2>
+//      <ul>
+//        {people.map(person => {
+//          return <li key={person.id}>{person.name}</li>
+//        })}
+//      </ul>
+//    </div>
+//  )
+//}
+
+//export default People
