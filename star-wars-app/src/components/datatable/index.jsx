29afa1ebd5b8e51972fae892f23dd384cbe66207
Elisabeth Droeser <elisabethdroeser@gmail.com>
2021-06-30 15:59:13 +0200

    Add css to table

--- star-wars-app/src/components/datatable/index.jsx
+++ star-wars-app/src/components/datatable/index.jsx
@@ -11,8 +11,25 @@ function Datatable({ data }) {
       <h2>Starwars Personer</h2>
       <ul className="list-group mb-4">
         {data.map(row => {
-          return <li key={row.id}>{row.name}</li>
-            })}
+          return (
+          <>
+            <div>
+              <section class="accordion">
+               <section key={row.id} style={myStyle} className="list-group-item">
+                <label for="cb-1">{row.name}</label>
+                <input id="cb_1" type="checkbox"></input>
+                <div>
+                  {row.birth_year}
+                </div>
+                <div>
+                  {row.gender}
+                </div>
+                </section>
+              </section>
+            </div>
+          </>
+          )
+          })}
         </ul>
     </div>
   )
